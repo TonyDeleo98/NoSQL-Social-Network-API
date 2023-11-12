7e9b0fdbc9fbd9cb1d48805949799d650c8fa272
TonyDeleo98 <Tonyd9819@gmail.com>
2023-11-12 14:43:52 -0500

    updated files

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
-uuidv4(); 
+uuidv4() 
 
 const router = express.Router();
 
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const user = req.body;
 
-    users.push(user);
+    users.push({ ... user, id: uuidv4() });
 
     res.send(`User with the name ${user.userName} added to the database!`);
 });
